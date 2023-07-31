5f8dc50e29b0c8086e5014f0a2dc20ed0cfb4c46
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-08-01 01:24:16 +0700

    style(swap page): change font-family

--- src/pages/MarketplacePage.tsx
+++ src/pages/MarketplacePage.tsx
@@ -1,5 +1,41 @@
 const MarketplacePage = () => {
-  return <div>MarketplacePage</div>;
+  const mockupData = [];
+  for (let i = 1; i <= 100; i++) {
+    mockupData.push({
+      id: i,
+      token: `Token ${i}`,
+      price: `$${Math.floor(Math.random() * 100)}`,
+      volume: `${Math.floor(Math.random() * 1000)}`,
+      change: `${Math.floor(Math.random() * 20) - 10}%`,
+    });
+  }
+
+  return (
+    <div className="container lg:max-w-screen-xl p-4 mx-auto bg-green-400">
+      <table className="bg-red-100 w-full text-center">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Token</th>
+            <th>Price</th>
+            <th>Volume</th>
+            <th>Change</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mockupData.map((data) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.token}</td>
+              <td>{data.price}</td>
+              <td>{data.volume}</td>
+              <td>{data.change}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default MarketplacePage;
--- src/pages/SwapPage.tsx
+++ src/pages/SwapPage.tsx
@@ -9,21 +9,21 @@ const SwapPage = () => {
 
   return (
     <div className="container lg:max-w-screen-xl p-4 mx-auto">
-      <div className="bg-white shadow-md space-y-4 max-w-sm mx-auto p-8 rounded-2xl border-2">
+      <div className="bg-white space-y-4 max-w-sm mx-auto p-8 rounded-2xl border-2 border-gray-200">
         <div className="space-x-4">
           <button
             onClick={() => handleActive("trade")}
             className={`${
-              active === "trade" ? "font-bold text-gray-900" : "text-gray-500"
-            } text-lg`}
+              active === "trade" ? "text-gray-900 font-medium" : "text-gray-500"
+            } text-lg font-BAI`}
           >
             Trade
           </button>
           <button
             onClick={() => handleActive("buy")}
             className={`${
-              active === "buy" ? "font-bold text-gray-900" : "text-gray-500"
-            } text-lg`}
+              active === "buy" ? "text-gray-900 font-medium" : "text-gray-500"
+            } text-lg font-BAI`}
           >
             Buy
           </button>
@@ -31,15 +31,15 @@ const SwapPage = () => {
         <div className="flex flex-col space-y-4">
           <input
             type="number"
-            className="border-2 p-2 text-3xl py-2 rounded-xl"
+            className="border-2 border-gray-200 p-2 text-3xl py-2 rounded-xl"
           />
           <input
             type="number"
-            className="border-2 p-2 text-3xl py-2 rounded-xl"
+            className="border-2 border-gray-200 p-2 text-3xl py-2 rounded-xl"
           />
         </div>
         <div>
-          <button className="bg-primary-color text-white w-full text-lg p-3 rounded-xl">
+          <button className="bg-primary-color text-white w-full text-lg p-3 rounded-xl font-BAI font-medium">
             {active === "trade" ? "Trade" : "Buy"}
           </button>
         </div>
