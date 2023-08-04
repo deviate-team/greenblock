6a09d97a383d54b7851f77fc7f9d423128a46972
mmenuu <rctkhtp@gmail.com>
2023-08-04 20:06:45 +0700

    fix(home): fix individual

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -59,13 +59,13 @@ const Feature = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                                 stroke="currentColor" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>
-                                Online transportation booking
+                                Online eco-friendly transportation booking
                             </li>
                             <li className="mb-4 flex">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                                 stroke="currentColor" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>
-                                Low carbon traveling
+                                Carbon footprint tracking
                             </li>
                             <li className="mb-4 flex">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
