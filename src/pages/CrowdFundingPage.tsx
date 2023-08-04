44483226e9182138184e6261880ed0bb1360c7de
mmenuu <rctkhtp@gmail.com>
2023-08-04 21:49:38 +0700

    fix(croedFunding): add step

--- src/pages/CrowdFundingPage.tsx
+++ src/pages/CrowdFundingPage.tsx
@@ -4,6 +4,11 @@ import { IProject } from "@/interfaces/project";
 import { projectService } from "@/services/projects.services";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  Stepper,
+  initTE,
+} from "tw-elements";
+initTE({ Stepper });
 
 const CrowdFundingPage = () => {
   const [projects, setProjects] = useState<IProject[]>([]);
@@ -34,8 +39,87 @@ const CrowdFundingPage = () => {
 
   return (
     <div className="container lg:max-w-screen-xl mx-auto p-4 space-y-6 font-BAI">
-      <h1 className="text-2xl font-BAI font-medium text-center">Crowdfunding</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+    <section>
+      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Crowd</span> Funding</h1>
+      <p className="text-xl font-BAI font-medium text-center text-gray-600">
+Drive sustainability and profitability for your small business through our Crowd Funding on Carbon Project! Offer carbon reduction projects and attract funding to earn valuable carbon credits for sale. Join now and make a real impact in the fight against climate change!</p>
+
+    <h3 className="text-2xl font-BAI font-medium text-center text-primary-color mt-10">How to invest</h3>
+      <ul
+        data-te-stepper-init
+        className="relative m-0 flex list-none justify-between overflow-hidden p-0 transition-[height] duration-200 ease-in-out">
+        <li
+          data-te-stepper-step-ref
+          data-te-stepper-step-active
+          className="w-[4.5rem] flex-auto">
+          <div
+            data-te-stepper-head-ref
+            className="flex cursor-pointer items-center pl-2 leading-[1.3rem] no-underline after:ml-2 after:h-px after:w-full after:flex-1 after:bg-[#e0e0e0] after:content-[''] hover:bg-[#f9f9f9] focus:outline-none dark:after:bg-neutral-600 dark:hover:bg-[#3b3b3b]">
+            <span
+              data-te-stepper-head-icon-ref
+              className="my-6 mr-2 flex h-[1.938rem] w-[1.938rem] items-center justify-center rounded-full bg-[#ebedef] text-sm font-medium text-[#40464f]">
+              1
+            </span>
+            <span
+              data-te-stepper-head-text-ref
+              className="font-medium text-neutral-500 after:flex after:text-[0.8rem] after:content-[data-content] dark:text-neutral-300">
+              step1
+            </span>
+          </div>
+          <div
+            data-te-stepper-content-ref
+            className="text-center text-gray-700 absolute w-full p-4 transition-all duration-500 ease-in-out">
+            Add money to your e-pocket
+          </div>
+        </li>
+
+        <li data-te-stepper-step-ref className="w-[4.5rem] flex-auto">
+          <div
+            data-te-stepper-head-ref
+            className="flex cursor-pointer items-center leading-[1.3rem] no-underline before:mr-2 before:h-px before:w-full before:flex-1 before:bg-[#e0e0e0] before:content-[''] after:ml-2 after:h-px after:w-full after:flex-1 after:bg-[#e0e0e0] after:content-[''] hover:bg-[#f9f9f9] focus:outline-none dark:before:bg-neutral-600 dark:after:bg-neutral-600 dark:hover:bg-[#3b3b3b]">
+            <span
+              data-te-stepper-head-icon-ref
+              className="my-6 mr-2 flex h-[1.938rem] w-[1.938rem] items-center justify-center rounded-full bg-[#ebedef] text-sm font-medium text-[#40464f]">
+              2
+            </span>
+            <span
+              data-te-stepper-head-text-ref
+              className="text-neutral-500 after:flex after:text-[0.8rem] after:content-[data-content] dark:text-neutral-300">
+              step2
+            </span>
+          </div>
+          <div
+            data-te-stepper-content-ref
+            className="text-center text-gray-700 absolute left-0 w-full translate-x-[150%] p-4 transition-all duration-500 ease-in-out">
+            Pick project you interested
+          </div>
+        </li>
+
+        <li data-te-stepper-step-ref className="w-[4.5rem] flex-auto">
+          <div
+            data-te-stepper-head-ref
+            className="flex cursor-pointer items-center pr-2 leading-[1.3rem] no-underline before:mr-2 before:h-px before:w-full before:flex-1 before:bg-[#e0e0e0] before:content-[''] hover:bg-[#f9f9f9] focus:outline-none dark:before:bg-neutral-600 dark:after:bg-neutral-600 dark:hover:bg-[#3b3b3b]">
+            <span
+              data-te-stepper-head-icon-ref
+              className="my-6 mr-2 flex h-[1.938rem] w-[1.938rem] items-center justify-center rounded-full bg-[#ebedef] text-sm font-medium text-[#40464f]">
+              3
+            </span>
+            <span
+              data-te-stepper-head-text-ref
+              className="text-neutral-500 after:flex after:text-[0.8rem] after:content-[data-content] dark:text-neutral-300">
+              step3
+            </span>
+          </div>
+          <div
+            data-te-stepper-content-ref
+            className="text-center absolute left-0 w-full translate-x-[150%] p-4 transition-all duration-500 ease-in-out text-gray-700">
+            Done the payment
+          </div>
+        </li>
+      </ul>
+    </section>
+    <section className = "mt-20">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-20">
         {projects.map((data) => (
           <Link
             to={`/crowdfunding/${data._id}`}
@@ -101,6 +185,7 @@ const CrowdFundingPage = () => {
           </Link>
         ))}
       </div>
+      </section>
     </div>
   );
 };
