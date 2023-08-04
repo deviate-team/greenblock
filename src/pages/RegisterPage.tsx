e7bdae3192625198a5c7c668c8c861b0cd74e861
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-08-04 12:29:21 +0700

    fix: fix form value in register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -104,7 +104,7 @@ const RegisterPage = () => {
                         <div className="w-full mb-6 md:mb-0">
                             <label className="block mb-2 text-sm font-medium text-gray-900">Phone Number</label>
                             <input type="text" placeholder="09X-XXX-XXX" className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-color focus:border-primary-color block w-full p-2.5"
-                                value={user.password}
+                                value={user.phoneNumber}
                                 onChange={(e) => {
                                     setUser({ ...user, phoneNumber: e.target.value });
                                     e.preventDefault();
