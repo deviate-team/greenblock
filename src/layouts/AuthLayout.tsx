f2e1d60e38085492cb6065561f54bf762617356e
khris-xp <khrisbharmmano@gmail.com>
2023-08-06 23:11:55 +0700

    feat: install toast and add a login toast

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { Toaster } from 'react-hot-toast';
 import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
     return (
         <div className="flex flex-col min-h-screen text-white">
             <Navbar />
+            <Toaster />
             <main className="flex-1 flex">
                 <Outlet />
             </main>
