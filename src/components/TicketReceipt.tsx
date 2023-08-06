aa36e40b17d544e499442e75b546e9bb65c6b860
khris-xp <khrisbharmmano@gmail.com>
2023-08-06 15:45:37 +0700

    feat(booking): transaction detail page and blog

--- src/components/TicketReceipt.tsx
+++ src/components/TicketReceipt.tsx
@@ -1,11 +1,34 @@
-const TicketReceipt = () => {
+import { formatDate, formatTime } from "@/hooks/format"
+import { ITransactionItem } from "@/interfaces/transaction"
+import { IUserProfile } from "@/interfaces/user"
+import { userService } from "@/services/user.services"
+import { useEffect, useState } from "react"
+
+const TicketReceipt = ({ transaction }: { transaction: ITransactionItem }) => {
+
+    const [user, setUser] = useState<IUserProfile>();
+
+    const fetchUser = async () => {
+        try {
+            const { data } = await userService.getUserProfile();
+            setUser(data);
+        }
+        catch (error) {
+            const message = (error as Error).message;
+            throw new Error(message);
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
     return (
         <div className="flex flex-col items-center justify-center mt-10">
             <div className="w-96 h-full mx-auto z-10 bg-primary-color rounded-3xl">
                 <div className="flex flex-col">
                     <div className="bg-white relative drop-shadow-2xl rounded-3xl p-4 m-4 font-BAI">
                         <div className="flex-none sm:flex">
-                            <div className=" relative h-32 w-32   sm:mb-0 mb-3 hidden">
+                            <div className="relative h-32 w-32 sm:mb-0 mb-3 hidden">
                                 <img src="https://tailwindcomponents.com/storage/avatars/njkIbPhyZCftc4g9XbMWwVsa7aGVPajYLRXhEeoo.jpg" alt="aji" className=" w-32 h-32 object-cover rounded-2xl" />
                                 <a href="#"
                                     className="absolute -right-2 bottom-2 -ml-3 text-white p-1 text-xs bg-primary-color hover:bg-primary-hover-color font-medium tracking-wider rounded-full transition ease-in duration-300">
@@ -21,65 +44,52 @@ const TicketReceipt = () => {
                                 <div className="flex items-center justify-between">
                                     <div className="flex items-center  my-1">
                                         <span className="mr-3 rounded-full bg-white w-8 h-8">
-                                            <img src="https://image.winudf.com/v2/image1/Y29tLmJldHMuYWlyaW5kaWEudWlfaWNvbl8xNTU0NTM4MzcxXzA0Mw/icon.png?w=&amp;fakeurl=1" className="h-8 p-1" />
+                                            <img src={transaction.user.imageProfile} className="h-8 p-1" />
                                         </span>
-                                        <h2 className="font-medium">Airindia</h2>
+                                        <h2 className="font-medium">{transaction.ticket.title}</h2>
                                     </div>
-                                    <div className="ml-auto text-primary-color">A380</div>
                                 </div>
                                 <div className="border-b border-dashed my-5"></div>
-                                <div className="flex items-center">
-                                    <div className="flex flex-col">
-                                        <div className="flex-auto text-xs text-gray-400 my-1">
-                                            <span className="mr-1 ">MO</span><span>19 22</span>
-                                        </div>
-                                        <div className="w-full flex-none text-lg text-primary-color font-bold leading-none">COK</div>
-                                        <div className="text-xs">Cochi</div>
-
-                                    </div>
-                                    <div className="flex flex-col mx-auto">
-                                        <img src="https://image.winudf.com/v2/image1/Y29tLmJldHMuYWlyaW5kaWEudWlfaWNvbl8xNTU0NTM4MzcxXzA0Mw/icon.png?w=&amp;fakeurl=1" className="w-20 p-1" />
-
-                                    </div>
-                                    <div className="flex flex-col ">
-                                        <div className="flex-auto text-xs text-gray-400 my-1">
-                                            <span className="mr-1">MO</span><span>19 22</span>
-                                        </div>
-                                        <div className="w-full flex-none text-lg text-primary-color font-bold leading-none">DXB</div>
-                                        <div className="text-xs">Dubai</div>
-
-                                    </div>
-                                </div>
-                                <div className="border-b border-dashed my-5 pt-5">
-                                    <div className="absolute rounded-full w-5 h-5 bg-primary-color -mt-2 -left-2"></div>
-                                    <div className="absolute rounded-full w-5 h-5 bg-primary-color -mt-2 -right-2"></div>
-                                </div>
                                 <div className="flex items-center mb-5 p-5 text-sm">
                                     <div className="flex flex-col">
-                                        <span className="text-sm">Flight</span>
-                                        <div className="font-semibold">Airbus380</div>
+                                        <span className="text-sm">Ticket Name</span>
+                                        <div className="font-semibold">{transaction.ticket.title}</div>
 
                                     </div>
                                     <div className="flex flex-col ml-auto">
-                                        <span className="text-sm">Gate</span>
-                                        <div className="font-semibold">B3</div>
+                                        <span className="text-sm">Ticket Description</span>
+                                        <div className="font-semibold">{transaction.ticket.description}</div>
+                                    </div>
+                                </div>
+                                <div className="flex items-center mb-8 px-5 text-center">
+                                    <div className="flex flex-col text-sm">
+                                        <span className="">Booking Date</span>
+                                        <div className="font-semibold">{formatDate(transaction.ticket.date)}</div>
+                                    </div>
+                                    <div className="flex flex-col mx-auto text-sm">
+                                        <span className="">Booking Time</span>
+                                        <div className="font-semibold">{formatTime(transaction.createdAt)}</div>
+
+                                    </div>
+                                    <div className="flex flex-col text-sm">
+                                        <span className="">Type</span>
+                                        <div className="font-semibold">{transaction.type}</div>
 
                                     </div>
                                 </div>
-                                <div className="flex items-center mb-4 px-5">
+                                <div className="flex items-center mb-4 mt-4 px-5 text-center">
                                     <div className="flex flex-col text-sm">
-                                        <span className="">Board</span>
-                                        <div className="font-semibold">11:50AM</div>
-
+                                        <span className="">Status</span>
+                                        <div className="font-semibold text-primary-color">{transaction.status}</div>
                                     </div>
                                     <div className="flex flex-col mx-auto text-sm">
                                         <span className="">Departs</span>
-                                        <div className="font-semibold">11:30Am</div>
+                                        <div className="font-semibold">{formatTime(transaction.ticket.depart_time)}</div>
 
                                     </div>
                                     <div className="flex flex-col text-sm">
                                         <span className="">Arrived</span>
-                                        <div className="font-semibold">2:00PM</div>
+                                        <div className="font-semibold">{formatTime(transaction.ticket.arrive_time)}</div>
 
                                     </div>
                                 </div>
@@ -87,20 +97,20 @@ const TicketReceipt = () => {
                                     <div className="absolute rounded-full w-5 h-5 bg-primary-color -mt-2 -left-2"></div>
                                     <div className="absolute rounded-full w-5 h-5 bg-primary-color -mt-2 -right-2"></div>
                                 </div>
-                                <div className="flex items-center px-5 pt-3 text-sm">
+                                <div className="flex items-center px-5 pt-3 text-sm text-center">
                                     <div className="flex flex-col">
                                         <span className="">Passanger</span>
-                                        <div className="font-semibold">Ajimon</div>
+                                        <div className="font-semibold">{user?.firstName} {user?.lastName}</div>
 
                                     </div>
                                     <div className="flex flex-col mx-auto">
-                                        <span className="">className</span>
-                                        <div className="font-semibold">Economic</div>
+                                        <span className="">Quantity</span>
+                                        <div className="font-semibold">{transaction.quantity} Seat</div>
 
                                     </div>
                                     <div className="flex flex-col">
-                                        <span className="">Seat</span>
-                                        <div className="font-semibold">12 E</div>
+                                        <span className="">Price</span>
+                                        <div className="font-semibold">{transaction.total_price} ฿</div>
 
                                     </div>
                                 </div>
---
+++ src/mocks/blog.test.ts
@@ -0,0 +1,32 @@
+import { IBlog } from "@/interfaces/blog"
+
+export const Blog: IBlog[] = [
+    {
+        "title": "Understanding Carbon Footprint: A Beginner's Guide",
+        "category": "Education",
+        "imageURL": "https://os.mreport.co.th/medias/module-experts/photo/Carbon-Credit-Electrical-and-Electronics-Institute.jpg",
+        "date": "2023-08-06",
+        "views": 1286
+    },
+    {
+        "title": "Top 10 Ways to Reduce Your Carbon Emissions",
+        "category": "Tips & Tricks",
+        "imageURL": "https://www.adheseal.com/wp-content/uploads/2022/04/Screen-Shot-2565-04-17-at-16.55.02.webp",
+        "date": "2023-08-05",
+        "views": 2074
+    },
+    {
+        "title": "The Role of Reforestation in Carbon Sequestration",
+        "category": "Environment",
+        "imageURL": "https://www.kaohoon.com/wp-content/uploads/2022/09/%E0%B8%84%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%9A%E0%B8%AD%E0%B8%99.jpg",
+        "date": "2023-08-04",
+        "views": 896
+    },
+    {
+        "title": "Carbon Credits: An Innovative Approach to Climate Change",
+        "category": "Climate Solutions",
+        "imageURL": "https://www.ananda.co.th/blog/thegenc/wp-content/uploads/2022/10/9_CarbonCreditCover01.jpg",
+        "date": "2023-08-03",
+        "views": 1567
+    },
+]
