c48f077a15c7437ddbf3e6c820eac9288ea30fad
khris-xp <khrisbharmmano@gmail.com>
2023-08-10 08:04:54 +0700

    fix: history detail path

--- src/pages/HistoryDetailPage.tsx
+++ src/pages/HistoryDetailPage.tsx
@@ -3,11 +3,10 @@ import TicketReceipt from "@/components/TicketReceipt";
 import { ITransactionItem } from "@/interfaces/transaction";
 import { transactionService } from "@/services/transaction.services";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
 const HistoryDetailPage = () => {
-    const [transaction, setTransaction] = useState<ITransactionItem>()
-    const { transactionId } = useParams<{ transactionId: string }>()
+    const [transaction, setTransaction] = useState<ITransactionItem>();
+    const transactionId = window.location.pathname.split("/")[2]
+
     const fetchTransactionById = async (id: string | undefined) => {
         const transactionById = await transactionService.getTransactionById(id)
         setTransaction(transactionById.data)
